30da6687ef61f092447e6677cec155d8af23a435
Haider Imtiaz <haider.imtiaz38@ee.ceme.edu.pk>
2024-09-28 18:29:21 +0500

    Decorator

---
+++ decorator_explamnation.py
@@ -0,0 +1,33 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+def calculate(cal_func, n1, n2):
+    return cal_func(n1, n2)
+
+
+# print(calculate(multiply, 9, 18))
+# print(calculate(divide, 9, 18))
+# print(calculate(subtract, 9, 18))
+# print(calculate(add, 9, 18))
+
+
+# def outer_function():
+#     print("I am outer")
+
+#     def nested_function():
+#         print("I am inner")
+#     nested_function()
+
+
+
+
