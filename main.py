106eabd308aa1ab73de5d29f8d50d4e883570f8c
Haider Imtiaz <haider.imtiaz38@ee.ceme.edu.pk>
2024-09-29 21:21:24 +0500

    New

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1 style = "text-align: center" >Hello World!</h1> <p> This is a paragraph </p> <img src=<iframe src="https://giphy.com/embed/tXL4FHPSnVJ0A" width="480" height="322" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kim-novak-tXL4FHPSnVJ0A">via GIPHY</a></p>'
+
+@app.route('/bye')
+def bye():
+    return 'Bye'
+
+@app.route('/<name>')
+def greet(name):
+    return f'Hello {name}'
+
 
 
 if __name__=="__main__":
